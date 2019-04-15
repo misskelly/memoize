a47ab506fa4076450a95bc680ea2e34c6a0f2276
Kelly Zick <kllyzck@gmail.com>
2019-04-15 10:46:17 -0600

    Pass Button snapshot test

--- src/card/button/Button.js
+++ src/card/button/Button.js
@@ -5,18 +5,20 @@ export default class Button extends Component {
   handleClick = (e) => {
     if (e.target.id === 'removeBtn') {
       this.props.removeFromDeck(this.props.cardId)
-    }
+    } 
+    // else if (e.target.id === 'wrongAnswerBtn'){
+    // }
   }
   render() {
     return (
       <>
         <button className='cardBtn' 
                 id={this.props.type}
-                onClick={this.handleClick}
-        >
+                onClick={this.handleClick}>
           {this.props.buttonText}
         </button>
-        <label htmlFor={this.props.type} className='btnLabel'>
+        <label  htmlFor={this.props.type} 
+                className='btnLabel'>
           {this.props.lable}
         </label>
       </>
--- src/card/button/Button.test.js
+++ src/card/button/Button.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Button from './Button';
+import { mockCurrentCard } from '../../mockData/mockData'
 import { shallow } from 'enzyme';
+
+const mockRemoveFromDeck = jest.fn();
 
 describe('Button', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <Button />
+      <Button removeFromDeck={mockRemoveFromDeck}
+              cardId={mockCurrentCard.cardId}  />
     )
   });
 
