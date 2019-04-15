0f329998a10f4bf98f0c10a9ecd0a4afd53b15cb
Kelly Zick <kllyzck@gmail.com>
2019-04-15 10:33:00 -0600

    Pass Card snapshot test

--- src/card/Card.test.js
+++ src/card/Card.test.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Card from './Card';
+import { mockCurrentCard } from '../mockData/mockData'
 import { shallow } from 'enzyme';
+
+
+const mockRemoveFromDeck = jest.fn();
+const mockGetRandomCard = jest.fn();
 
 describe('Card', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <Card />
+      <Card cardId={mockCurrentCard.cardNumber}
+            term={mockCurrentCard.term}
+            definition={mockCurrentCard.definition}
+            getRandomCard={mockGetRandomCard}
+            removeFromDeck={mockRemoveFromDeck} />
+            // addDuplicate={mockCurrentCard.addDuplicate} />
     )
   });
 
